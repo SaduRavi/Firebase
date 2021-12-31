170a893e56018c79c32f2166fdc989db539c0b21
Sadurshan Ravindran <91135027+SaduRavi@users.noreply.github.com>
2021-12-31 17:27:46 +0530

    added to add and view from db

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import "./styles.css";
 import db from "./Firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [todos, setTodo] = useState([]);
   const ref = collection(db, "todos");
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(ref);
-      console.log(data);
+      setTodo(data.docs.map((doc) => ({ ...doc.data() })));
     };
     getUsers();
   }, []);
-  console.log(db);
+
+  var add = "";
+  function getTodo(value) {
+    add = value.target.value;
+  }
+  const addtodo = async () => {
+    await addDoc(ref, { todo: add });
+    window.location.reload();
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox sadu</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happeuserInputn!</h2>
+      <input onChange={getTodo} />
+      <button onClick={addtodo}> Click me </button>
+      <ul className="todoList">
+        {todos.map((todo) => (
+          <li> {todo.todo} </li>
+        ))}
+        ;
+      </ul>
     </div>
   );
 }
